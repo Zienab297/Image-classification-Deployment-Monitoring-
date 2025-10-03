8fabcde2d8dd126e65ecd15da5e97a2e5ddf9e73
Zainab Ehab <60106900+Zienab297@users.noreply.github.com>
2025-10-03 18:31:20 +0300

    Add files via upload

---
+++ serializeImageData.py
@@ -0,0 +1,31 @@
+import json
+import boto3
+import base64
+
+s3 = boto3.resource("s3")
+
+def lambda_handler(event, context):
+    """A function to serialize target data from S3"""
+    
+    # Get the s3 address from the Step Function event input
+    key =  "test/bicycle_s_000513.png"
+    bucket = "sagemaker-us-east-1-329599635419"
+    
+    # Download the data from s3 to /tmp/image.png
+    s3.Bucket(bucket).download_file(key, '/tmp/image.png')
+    
+    # We read the data from a file
+    with open("/tmp/image.png", "rb") as f:
+        image_data = base64.b64encode(f.read())
+
+    # Pass the data back to the Step Function
+    print("Event:", event.keys())
+    return {
+        'statusCode': 200,
+        'body': {
+            "image_data": image_data,
+            "s3_bucket": bucket,
+            "s3_key": key,
+            "inferences": []
+        }
+    }
